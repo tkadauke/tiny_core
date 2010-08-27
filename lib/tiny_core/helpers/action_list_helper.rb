8a79ee12b3bedd6922a28a53ccb8d89ca50d98b0
Thomas Kadauke <tkadauke@imedo.de>
2010-08-27 02:41:29 +0200

    added action list helper

---
+++ lib/tiny_core/helpers/action_list_helper.rb
@@ -0,0 +1,27 @@
+module ActionListHelper
+  def action_list(&block)
+    yield builder = ActionListBuilder.new(self)
+    concat(builder.build!)
+  end
+  
+  class ActionListBuilder
+    def initialize(template)
+      @template = template
+      @items = []
+    end
+    
+    def method_missing(method, *args)
+      if @template.respond_to?(method)
+        @items << @template.send(method, *args)
+      else
+        super
+      end
+    end
+    
+    def build!
+      @items.join(' | ')
+    end
+  end
+end
+
+ActionController::Base.send :helper, TinyCore::ActionListHelper
